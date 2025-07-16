1bcdb7688f7cfc8b06faa1f9d6337ceeaa00145e
benny506 <olomufeh@gmail.com>
2025-07-16 21:40:28 +0100

    updates

--- src/pages/BPOSolutions.tsx
+++ src/pages/BPOSolutions.tsx
@@ -12,8 +12,6 @@ const BPOSolutions = () => {
   return (
     <>
       <section className="lg:pt-[8rem] pt-[7rem] pb-[3rem] lg:px-[3rem] px-[1rem] bg-[url('/static/header-pattern.png')] bg-top bg-no-repeat">
-        {/* <p>Testing</p> */}
-
         <BPO_Header />
 
         <div className="my-5 py-12" />  
